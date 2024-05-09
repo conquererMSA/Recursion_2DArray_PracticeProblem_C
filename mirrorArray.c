97a796c57c8ce3f74e9428b2c450ec6b1b4ee6fa
asus test <msa-99@outlook.com>
2024-05-09 23:58:21 +0600

    findMax by recursion

--- mirrorArray.c
+++ mirrorArray.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
+#include<limits.h>
+int findMax(int a[], int len, int i){
+    if(i==len) return INT_MIN;
+    int max=a[i];
+    int ans=findMax(a, len, i+1);
+    if(ans>max) max=ans;
+    return max;
+}
 int main(){
-    
+    int len;
+    scanf("%d",&len);
+    int a[len];
+    for(int i=0; i<len; i++){
+        scanf("%d",&a[i]);
+    }
+    int maxNum=findMax(a,len,0);
+    printf("%d",maxNum);
     return 0;
 }
